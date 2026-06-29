a0a81501983407e6edd1a01b3fd4a06361b55a3b
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2026-06-29 17:30:23 +0900

    feat: 貸借一致の検証(借方==貸方)を追加、不一致の仕訳を弾く

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,3 +1,16 @@
 class JournalEntry < ApplicationRecord
     has_many :journal_lines, dependent: :destroy
+
+    validate :debit_credit_must_balance
+
+    private
+
+    def debit_credit_must_balance
+        debit  = journal_lines.select { |l| l.side == "debit"  }.sum { |l| l.amount }
+        credit = journal_lines.select { |l| l.side == "credit" }.sum { |l| l.amount }
+
+        if debit != credit
+            errors.add(:base, "貸借が一致しません（借方#{debit} / 貸方#{credit}）")
+        end
+    end
 end
