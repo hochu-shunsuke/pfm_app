bef0748a853634b710a316f00bf562ddb7ff260c
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2026-06-30 11:19:30 +0900

    chore: dev/testDBをSQLiteからPostgreSQL(Docker)へ移行

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_06_29_063503) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string "category", null: false
     t.datetime "created_at", null: false
